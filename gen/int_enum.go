9fc1097ff8aa8c13022d8c1ccf966c77856325fa
Jea杨 <jjonline@jjonline.cn>
2021-12-13 17:24:07 +0800

    doc

--- gen/int_enum.go
+++ gen/int_enum.go
@@ -2,6 +2,8 @@ package gen
 
 //go:generate echo "first"
 
+// 通过为自定义类型Code的值定义时给注释备注而指定该code对应的文案
+// int_x_gen.go 即为生成的go源码文件，可以看到里面实现了fmt.String接口
 const (
 	CodeErr      Code = iota + 1 // 错误
 	CodeMsg                      // 提示
--- gen/int_type.go
+++ gen/int_type.go
@@ -3,4 +3,8 @@ package gen
 //go:generate $GOPATH/bin/stringer -type Code -linecomment  -output int_x_gen.go
 //go:generate ls -ll
 //go:generate echo "two"
+
+// Code 自定义INT型类型
+// 通过stringer指令为Code添加了String方法实现
+// 那么直接通过fmt.Println(c)时会调用输出String方法而不是c值本身
 type Code int
